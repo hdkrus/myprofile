c3da55b14b7351334aca93cf616ea9d5ebd7651f
Roberto Martinez <hdkrus@gmail.com>
2026-05-05 18:53:58 -0400

    add linux distros specificities

--- src/os/.linux_based.sh
+++ src/os/.linux_based.sh
@@ -2,9 +2,56 @@
 
 . "$OS_DIR/.unix_based.sh"
 
+function linux-release() {
+  cat /etc/os-release
+}
+
+function linux-id() {
+  local id=$(linux-release | \grep ^ID=)
+
+  if [[ "$id" =~ ^ID=(.+) ]]; then
+    echo ${BASH_REMATCH[1]}
+  else
+    echo "<Unknown>"
+  fi
+}
+
+function linux-id-based() {
+  local like=$(linux-release | \grep ^ID_LIKE=)
+  
+  if [[ "$like" =~ ^ID_LIKE=(.+) ]]; then
+    echo ${BASH_REMATCH[1]}
+  else
+    linux-id
+  fi
+}
+
 function update-os() {
-  echo "Updating linux..."
+  local based=$(linux-id-based)
+  if [[ "$based" =~ *"debian"* ]]; then
+    update-debian-based
+  elif [[ "$based" =~ *"fedora"* ]]; then
+    update-fedora-based
+  elif [[ "$based" =~ *"arch"* ]]; then
+    update-arch-based
+  else
+    error "Not supported update for distro based in $based"
+  fi
+}
+
+function update-debian-based() {
+  echo "Updating debian based linux..."
   sudo apt-get update
   sudo apt-get upgrade --yes
   sudo apt autoremove
+}
+
+function update-fedora-based() {
+  echo "Updating fedora based linux..."
+  sudo dnf upgrade -y
+}
+
+function update-fedora-based() {
+  echo "Updating arch based linux..."
+  sudo pacman -Syu
 }
