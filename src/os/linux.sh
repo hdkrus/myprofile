2f4fbce98bfe1e56e1edd8908f450edbe5dae522
Roberto Martinez <hdkrus@gmail.com>
2026-05-16 14:56:08 -0400

    improve linux foundation

--- src/os/linux.sh
+++ src/os/linux.sh
@@ -1,8 +1,62 @@
 #!/bin/bash
 
-. "$OS_DIR/.linux_based.sh"
+. "$OS_DIR/.unix_based.sh"
 
 # clipboard commands
 alias pbcopy='xclip -selection clipboard'
 
 alias pbpaste='xclip -selection clipboard -o'
+
+function linux-release() {
+  cat /etc/os-release
+}
+
+function linux-id() {
+  local id=$(linux-release | \grep ^ID=)
+
+  if [[ "$id" =~ ^ID=(.+) ]]; then
+    echo ${BASH_REMATCH[1]}
+  else
+    echo "<Unknown>"
+  fi
+}
+
+function linux-id-based() {
+  local like=$(linux-release | \grep ^ID_LIKE=)
+
+  if [[ "$like" =~ ^ID_LIKE=(.+) ]]; then
+    echo ${BASH_REMATCH[1]}
+  else
+    linux-id
+  fi
+}
+
+function update-os() {
+  local based=$(linux-id-based)
+  if [[ "$based" =~ *"debian"* ]]; then
+    update-debian-based
+  elif [[ "$based" =~ *"fedora"* ]]; then
+    update-fedora-based
+  elif [[ "$based" =~ *"arch"* ]]; then
+    update-arch-based
+  else
+    error "Not supported update for distro based in $based"
+  fi
+}
+
+function update-debian-based() {
+  echo "Updating debian based linux..."
+  sudo apt-get update
+  sudo apt-get upgrade --yes
+  sudo apt autoremove
+}
+
+function update-fedora-based() {
+  echo "Updating fedora based linux..."
+  sudo dnf upgrade -y
+}
+
+function update-arch-based() {
+  echo "Updating arch based linux..."
+  sudo pacman -Syu --noconfirm
+}
