b9d46dbd39684d34457e3efe8f907d5b4dedfefe
Roberto Martinez <hdkrus@gmail.com>
2026-05-27 14:13:21 -0400

    fix k8s tool

--- src/tools/load-tools.sh
+++ src/tools/load-tools.sh
@@ -10,8 +10,13 @@ function load-tool() {
 
 function load-tool-if-exists() {
     local tool="$1"
+    local cmd="$2"
+
+    if [ -z "$cmd" ]; then
+      cmd="$tool"
+    fi
     
-    if command -v "$tool" &> /dev/null; then
+    if command -v "$cmd" &> /dev/null; then
         load-tool "$tool"
     fi
 }
@@ -22,7 +27,7 @@ function load-existing-tools() {
     load-tool-if-exists brew
     load-tool-if-exists flatpak
     load-tool-if-exists snap
-    load-tool-if-exists k8s
+    load-tool-if-exists k8s kubectl
 }
 
 load-existing-tools
